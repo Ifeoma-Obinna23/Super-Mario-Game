12c6e7fdd668a04b6513fbac6416178870c96dce
Ifeoma-Obinna23 <ifeomaohachosim@yahoo.com>
2024-10-18 01:06:15 +0100

    updated my role

--- EKS-TF/main.tf
+++ EKS-TF/main.tf
@@ -11,14 +11,14 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
-resource "aws_iam_role" "example" {
+resource "aws_iam_role" "example1" {
   name               = "eks-cluster-cloud"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
 resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.example.name
+  role       = aws_iam_role.example1.name
 }
 
 #get vpc data
@@ -35,7 +35,7 @@ data "aws_subnets" "public" {
 #cluster provision
 resource "aws_eks_cluster" "example" {
   name     = "EKS_CLOUD"
-  role_arn = aws_iam_role.example.arn
+  role_arn = aws_iam_role.example1.arn
 
   vpc_config {
     subnet_ids = data.aws_subnets.public.ids
